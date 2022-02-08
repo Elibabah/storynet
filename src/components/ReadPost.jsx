e54f20548c59936fae59256e4c1a8356c6af0848
Elibabah <delias16.hernandez@gmail.com>
2022-02-08 16:00:10 -0600

    CRUD reading

--- src/components/ReadPost.jsx
+++ src/components/ReadPost.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { updateDoc, doc} from "firebase/firestore"
+import { db } from "../firebase"
 
-export const ReadPost = ( {arrayPublicaciones} ) => {
+export const ReadPost = ( {arrayPublicaciones, correoUsuario, setArrayPublicaciones} ) => {
+
+      async function eliminarPublicacion(idPublicacionAEliminar){
+            //crear nuevo Array de tareas
+            const newArrayPublicaciones = arrayPublicaciones.filter(
+                  (objetoPublicacion)=> objetoPublicacion.id !== idPublicacionAEliminar
+            );
+            //actualizar base de datos
+            const docuRef = doc(db, `social/${correoUsuario}`);
+            updateDoc(docuRef, { publicaciones: [...newArrayPublicaciones]});
+            //actualizar state
+            setArrayPublicaciones(newArrayPublicaciones);
+      }
+
       return(
             <div>
                   <h1>Hola, estás leyendo el feed</h1>
                   <div>
                         {arrayPublicaciones.map((objetoPublicacion) => {
                               return (
-                                    <div>
-                                          <div>{objetoPublicacion.title}</div>
+                                    <div key={objetoPublicacion.id}> 
+                                          <div key={objetoPublicacion.id}>{objetoPublicacion.title}</div>
                                           <div>{objetoPublicacion.story}</div>
-                                          <button>Delete</button>
+                                          <button onClick={()=> eliminarPublicacion(objetoPublicacion.id)}>Delete</button>
                                     </div>
 
                               )
