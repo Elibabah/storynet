26eac8c10b7a724af6bb6f55f013ca9214badce2
Elibabah <delias16.hernandez@gmail.com>
2022-02-14 13:55:40 -0600

    update doesn't work

--- src/components/ReadPost.jsx
+++ src/components/ReadPost.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { updateDoc, doc} from "firebase/firestore"
+import { updateDoc, doc, getDoc, setDoc} from "firebase/firestore"
 import { db } from "../firebase"
 // READ
 export const ReadPost = ( {arrayPublicaciones, correoUsuario, setArrayPublicaciones, publicaciones} ) => {
@@ -16,9 +16,21 @@ export const ReadPost = ( {arrayPublicaciones, correoUsuario, setArrayPublicacio
             setArrayPublicaciones(newArrayPublicaciones);
       }
 
+      async function editarPublicacion(idPublicacionAEditar, title, story){
+            // obtener ID 
 
-      async function editarPublicacion(idPublicacionAEditar){
-            //editar
+            
+            const docuRef = doc(db, `social/${idPublicacionAEditar}`)
+
+            //////////////////////////////////////
+            //buscar documento
+            const consulta = await getDoc(docuRef);
+            //revisar si existe 
+            if (consulta.exists()) {
+            //Si existe:
+            const infoDocu = consulta.data()
+            console.log(infoDocu.publicaciones)}
+            console.log(consulta.data())
       }
 
       return(
@@ -32,6 +44,16 @@ export const ReadPost = ( {arrayPublicaciones, correoUsuario, setArrayPublicacio
                                           <div>{objetoPublicacion.story}</div>
                                           <button onClick={()=> eliminarPublicacion(objetoPublicacion.id)}>Delete</button>
                                           <button onClick={()=> editarPublicacion(objetoPublicacion.id)}>Edit</button>
+                                                <div>      
+                                                      <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+
+                                                            <input type="text" id="title" placeholder="título" className='shadow appareance-non border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
+
+                                                            <input type="text" id="story" placeholder="Escribe tu historia" className='shadow appareance-non border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
+
+                                                            <button className='bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Guardar edición</button>
+                                                </form> 
+                                          </div>
                                     </div>
 
                               )
